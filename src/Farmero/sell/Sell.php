d810b5c37e4ec2ef9e3e93ac2908ca204291c912
Farmero14 <145240944+Farmero14@users.noreply.github.com>
2024-05-28 12:27:41 -0700

    Update Sell.php

--- src/Farmero/sell/Sell.php
+++ src/Farmero/sell/Sell.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Farmero\sell;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 
 use Farmero\sell\Commands\SellCommand;
 use Farmero\sell\Commands\SellAllCommand;
@@ -15,8 +16,13 @@ class Sell extends PluginBase {
         $this->saveResource("items.yml");
 
         $this->getServer()->getCommandMap()->registerAll("Sell", [
-			new SellCommand($this),
-			new SellAllCommand($this)
-		]);
-    }
+	    new SellCommand($this),
+	    new SellAllCommand($this)
+	    ]);
+        }
+	if (Server::getInstance()->getPluginManager()->getPlugin("MoneySystem") === null) {
+            $this->getLogger()->info("Disabling Sell, MoneySystem not found... Please make sure to have it installed before trying again!");
+            Server::getInstance()->getPluginManager()->disablePlugin($this);
+            return;
+        }
 }
